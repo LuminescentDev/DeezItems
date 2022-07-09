be4c9f154370087316b1bdfc500d69513f7f66de
AkiraDev <akira.devjs@gmail.com>
2022-07-08 22:33:32 -0700

    forgot to commit block event

---
+++ src/main/java/xyz/akiradev/deezitems/events/block/EventBlockBreak.java
@@ -0,0 +1,31 @@
+package xyz.akiradev.deezitems.events.block;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+import xyz.akiradev.deezitems.utils.DeezItem;
+import xyz.akiradev.deezitems.utils.ItemUtils;
+
+public class EventBlockBreak implements Listener {
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
+    public void onBlockBreak(BlockBreakEvent event) {
+        if(ItemUtils.isDeez(event.getPlayer().getInventory().getItemInMainHand())){
+            useItem(event, event.getPlayer().getInventory().getItemInMainHand());
+        }
+    }
+
+    private void useItem(BlockBreakEvent event, ItemStack item) {
+        Player player = event.getPlayer();
+        DeezItem deezItem = ItemUtils.getDeezItem(item);
+        if (deezItem != null) {
+            if(deezItem.breakBlockAction(player, event, event.getBlock(), item)){
+                deezItem.onItemUse(player, item);
+            }
+        }
+    }
+
+}
